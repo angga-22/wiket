7f325a5feafb77ad148fd24a7a7a9610f954bc9c
carolinmaisenbacher <carolin.maisenbacher@code.berlin>
2020-03-21 01:14:01 +0100

    Create Default Footer It contains link to legal pages and to the Puzzlers website

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -2,5 +2,34 @@
 import React, { memo } from 'react';
 // pieces components
 import { Section } from 'pieces/layout';
+import { GatsbyLink } from 'pieces/elements';
+import { Link } from 'pieces/typography/elements';
+import { GridItem } from 'pieces/grid';
+import { Paragraph } from 'pieces/typography';
 
-export const Footer = memo(() => <Section as='footer' />);
+export const Footer = memo(() => (
+  <Section as='footer' sx={{ mb: ['20vw', '10vw', '0', '0'] }}>
+    <GridItem
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 1, duration: 2 } }}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        margin: ['10vw 0 4vw', '4vw 0', '4vw 0', '3vw 0', '2vw 0'],
+      }}
+    >
+      <GatsbyLink to='/datenschutz' variant='footer'>
+        Datenschutz
+      </GatsbyLink>
+      <GatsbyLink sx={{ marginLeft: ['5vw'] }} to='/impressum' variant='footer'>
+        Impressum
+      </GatsbyLink>
+      <Paragraph sx={{ marginLeft: ['5vw'] }}>
+        {'Build with <3 by '}
+        <Link variant='hyperLink' href='https://thepuzzlers.io'>
+          The Puzzlers.
+        </Link>
+      </Paragraph>
+    </GridItem>
+  </Section>
+));
