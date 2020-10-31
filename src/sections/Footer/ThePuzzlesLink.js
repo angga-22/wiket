f45222ddbcddcd19e9a1517e4418e5a4c843f819
carolinmaisenbacher <carolin.maisenbacher@code.berlin>
2020-10-31 09:44:19 +0000

    Refactor Footer Sections
    It now has a simple layout that can be used by most single page websites.
    We also added two different versions: One for a single language website and one Footer
    (that is currently outcommented) that shows how multiple langauges could be handled.

---
+++ src/sections/Footer/ThePuzzlesLink.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+// Pieces Imports
+import { usePageContext } from 'pieces/utils/context';
+import { FlexWrapper } from 'pieces/flex';
+import { Paragraph } from 'pieces/typography';
+import { Image } from 'pieces/elements';
+
+export const ThePuzzlersLink = () => {
+  const { locale } = usePageContext();
+  const { puzzlersLogo } = useStaticQuery(graphql`
+    query {
+      puzzlersLogo: file(relativePath: { eq: "puzzlers-logo.svg" }) {
+        id
+        publicURL
+      }
+    }
+  `);
+
+  return (
+    <a
+      href={
+        locale === 'de'
+          ? 'https://thepuzzlers.io/de/'
+          : 'https://thepuzzlers.io/'
+      }
+      style={{ cursor: 'pointer' }}
+    >
+      <FlexWrapper sx={{ flexDirection: 'column' }}>
+        <Paragraph sx={{ textAlign: 'left' }}>
+          {locale === 'de' ? 'Eine Webseite von' : 'A website by'}
+        </Paragraph>
+
+        <Image
+          sx={{
+            height: ['30px', '30px'],
+          }}
+          src={puzzlersLogo.publicURL}
+          alt='Logo of The Puzzles'
+        />
+      </FlexWrapper>
+    </a>
+  );
+};
