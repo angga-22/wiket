1ab993c62fcc4e8b0c40b8daa825d32f2f692317
Carolin Maisenbacher <32734844+carolinmaisenbacher@users.noreply.github.com>
2021-04-18 21:40:18 +0800

    Update breakpoint array
    We are switching from a 5 breakpoint concept to 6 breakpoints.
    This allows us to not couple our design to devices like phone and tablet, etc.
    But instead be able to handle any device.

--- src/pieces/theme/breakpoints.js
+++ src/pieces/theme/breakpoints.js
@@ -1,12 +1,28 @@
+/* eslint-disable max-len */
 export const breakpoints = [
-  // Portrait
+  // Portrait - is added by theme-ui automatically
   // '@media screen and (min-width: 20em)', // 320px phone_portrait
-  '@media screen and (min-width: 32em) and (orientation: landscape)', // 512px phone_landscape
-  '@media screen and (min-width: 40em) and (orientation: portrait)', // 720px tablet_portrait
-  // Landscape
-  '@media screen and (min-width: 63em) and (orientation: landscape)', // 1024px tablet_landscape
-  '@media screen and (min-width: 80em)', // 1200px desktop_s
-  '@media screen and (min-width: 160em)', // 2560px desktop_xxl
+
+  // Small
+  '@media screen and (min-width: 500px)',
+
+  // Medium
+  '@media screen and (min-width: 750px) and (orientation: portrait)',
+  '@media screen and (min-width: 700px) and (orientation: landscape)',
+
+  // Large
+  '@media screen and (min-width: 1000px)',
+
+  // Xlarge
+  '@media screen and (min-width: 1400px)',
 ];
 
-export const pixelValues = [320, 512, 640, 1024, 1280];
+export const pixelValues = [320, 500, 750, 700, 1000, 1400];
+
+export const srcsetValues = [
+  '(min-width: 500px)',
+  '(min-width: 750px) and (orientation: portrait)',
+  '(min-width: 700px) and (orientation: landscape)',
+  '(min-width: 1000px)',
+  '(min-width: 1400px)',
+];
