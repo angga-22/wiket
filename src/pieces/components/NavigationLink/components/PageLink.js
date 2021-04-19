8d6275a9d0ad4c763c1a5e1f854206706c69b21d
Carolin Maisenbacher <32734844+carolinmaisenbacher@users.noreply.github.com>
2021-04-19 12:19:23 +0800

    Makie PageLink work with localized pages

--- src/pieces/components/NavigationLink/components/PageLink.js
+++ src/pieces/components/NavigationLink/components/PageLink.js
@@ -3,14 +3,20 @@ import { useLocation } from '@gatsbyjs/reach-router';
 import PropTypes from 'prop-types';
 // Pieces imports
 import { GatsbyLink } from '../../GatsbyLink';
+import { usePageContext } from '../../../utils/context';
+import { localizePath } from '../../../utils/functions';
 
 // Uses GatsbyLink, to navigate to a new page
 export const PageLink = ({ children, to, sx = {}, variant = 'navigation' }) => {
   const location = useLocation();
+  const { locale } = usePageContext();
 
-  const isActive = location.pathname === to || location.pathname === `${to}/`;
+  const localizedTo = localizePath(to, locale);
+  const isActive =
+    location.pathname === localizedTo ||
+    location.pathname === `${localizedTo}/`;
   return (
-    <GatsbyLink active={isActive} variant={variant} sx={sx} to={to}>
+    <GatsbyLink active={isActive} variant={variant} sx={sx} to={localizedTo}>
       {children}
     </GatsbyLink>
   );
