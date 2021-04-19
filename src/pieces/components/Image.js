f4dc7af073a7145d570cdd7ff25d77668dfa9518
Carolin Maisenbacher <32734844+carolinmaisenbacher@users.noreply.github.com>
2021-04-19 12:07:48 +0800

    Add default styling
    Also added PropTypes

--- src/pieces/components/Image.js
+++ src/pieces/components/Image.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { motion } from 'framer-motion';
 import { Box } from './Box';
 
-export const Image = React.forwardRef((props, ref) => (
-  <Box ref={ref} as={motion.img} {...props} />
+export const Image = React.forwardRef(({ sx, ...props }, ref) => (
+  <Box
+    ref={ref}
+    as={motion.img}
+    {...props}
+    sx={{ width: '100%', zIndex: 'inherit', ...sx }}
+  />
 ));
+
+Image.propTypes = {
+  sx: PropTypes.shape(),
+};
+
+Image.defaultProps = {
+  sx: {},
+};
