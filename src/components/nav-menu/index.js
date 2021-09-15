890668a04a7729b56fea06547d5b3ba6a449331a
angga-22 <anggasp.unib@gmail.com>
2021-09-15 18:56:03 +0800

    develop navigation

--- src/components/nav-menu/index.js
+++ src/components/nav-menu/index.js
@@ -10,17 +10,18 @@ export const Nav = () => (
   <Section
     id='navigation-section'
     sx={{
-      height: ['65px', '65px', '80px', '80px', '100px', '100px'],
+      // height: [null, null, null, null, '100px', '100px'],
       top: '0',
       position: 'fixed',
       zIndex: '2',
       backgroundColor: 'background',
+      boxShadow: '0px 18px 24px rgba(0, 0, 0, 0.1)',
     }}
   >
     <Logo />
-    <NavigationLinks />
-    <HamburgerMenu />
-    <GetStartedButton />
+    <DesktopNavigationLinks />
+    <MobileNavigation />
+    {/* <GetStartedButton /> */}
   </Section>
 );
 
@@ -29,15 +30,6 @@ const Logo = () => (
     sx={{
       gridColumn: ['1 / 5', '1 /4', '1 /  6', '1 /  5', '1 /  4', '1 /  4'],
       alignSelf: 'center',
-
-      transform: [
-        'translateY(50%)',
-        'translateY(50%)',
-        'translateY(50%)',
-        'translateY(60%)',
-        'translateY(5%)',
-        'translateY(10%)',
-      ],
     }}
   >
     <Link to='/'>
@@ -55,7 +47,7 @@ const Logo = () => (
   </GridItem>
 );
 
-const NavigationLinks = () => {
+const DesktopNavigationLinks = () => {
   const data = useStaticQuery(graphql`
     query getNavData {
       nav: navJson {
@@ -72,7 +64,7 @@ const NavigationLinks = () => {
         gridColumn: [null, null, null, null, '10 / 21', '11 / 21'],
         display: ['none', 'none', 'none', 'none', 'flex', 'flex'],
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: '',
         alignSelf: 'center',
       }}
     >
@@ -90,103 +82,127 @@ const NavigationLinks = () => {
   );
 };
 
-const GetStartedButton = () => (
+const MobileNavigation = () => (
   <GridItem
     sx={{
       gridColumn: [
-        '6/ 11',
-        '8 / 11',
-        '18 / 23',
-        '18 / 23',
-        '21 / 25',
-        '22/ 25',
-      ],
-      alignSelf: 'center',
-      display: ['flex'],
-      transform: [
-        'translateY(-60%)',
-        'translateY(-60%)',
-        'translateY(-50%)',
-        'translateY(-50%)',
-        'translateY(0)',
-        'translateY(0)',
-      ],
-    }}
-  >
-    <Button
-      variant='paragraph'
-      sx={{
-        width: ['100%', '100%', '93%', '85%', '100%', '100%'],
-        ml: ['15px', '0', null, null, '38px', '20px'],
-        borderRadius: '10px',
-        height: ['40px', '42px', '42px', '40px', '42px', '42px'],
-        backgroundColor: 'buttonBackground',
-        fontSize: '12px',
-        color: '#fff',
-        fontWeight: 'bold',
-      }}
-    >
-      Get Started
-    </Button>
-  </GridItem>
-);
-
-const HamburgerMenu = () => (
-  <GridItem
-    sx={{
-      backgroundColor: 'background',
-      gridColumn: [
-        '11 / 13',
+        '6 / 13',
         '12',
         '23 / 25',
         ' 23 / 25',
         '1 / span 24',
         '1 / span 24',
       ],
-      display: ['block', 'block', 'block', 'block', 'none', 'none'],
+      display: ['flex', 'block', 'block', 'block', 'none', 'none'],
+      justifyContent: 'right',
       cursor: 'pointer',
-      transform: [
-        'translateY(60%)',
-        'translateY(55%)',
-        'translateY(70%)',
-        'translateY(70%)',
-      ],
+      p: ['20px 0'],
+    }}
+  >
+    <Button
+      variant='paragraph'
+      sx={{
+        // width: '100%',
+        // width: ['100%', '100%', '93%', '85%', '100%', '100%'],
+        // ml: ['15px', '0', null, null, '38px', '20px'],
+        borderRadius: '10px',
+        backgroundColor: 'buttonBackground',
+        fontSize: '16px',
+        color: '#fff',
+        fontWeight: 'bold',
+        p: ['12 16'],
+      }}
+    >
+      Get Started
+    </Button>
+    <HamburgerMenu />
+  </GridItem>
+);
+
+// const GetStartedButton = () => (
+//   <GridItem
+//     sx={{
+//       gridColumn: [
+//         '6/ 11',
+//         '8 / 11',
+//         '18 / 23',
+//         '18 / 23',
+//         '21 / 25',
+//         '22/ 25',
+//       ],
+//       alignSelf: 'center',
+//       display: ['flex'],
+
+//     }}
+//   >
+//     <Button
+//       variant='paragraph'
+//       sx={{
+//         width: ['100%', '100%', '93%', '85%', '100%', '100%'],
+//         ml: ['15px', '0', null, null, '38px', '20px'],
+//         borderRadius: '10px',
+//         height: ['40px', '42px', '42px', '40px', '42px', '42px'],
+//         backgroundColor: 'buttonBackground',
+//         fontSize: '12px',
+//         color: '#fff',
+//         fontWeight: 'bold',
+//       }}
+//     >
+//       Get Started
+//     </Button>
+//   </GridItem>
+// );
+
+const HamburgerMenu = () => (
+  <Box
+    sx={{
+      cursor: 'pointer',
+      height: '40px',
+      width: '40px',
+      display: 'flex',
+      alignItems: 'center',
+      ml: '18px',
     }}
   >
     <Box
       sx={{
-        cursor: 'pointer',
-        mb: '5px',
-        width: ['55%', '80%', '55%', '50%'],
-        ml: ['45%', '20%', '45%', '50%'],
-        height: '4px',
-        backgroundColor: 'primary',
-        borderRadius: '4px',
-      }}
-    />
-    <Box
-      sx={{
-        cursor: 'pointer',
-        mb: '5px',
-        width: ['70%', '100%', '70%', '66%'],
-        ml: ['30%', '0', '30%', '34%'],
-        height: '4px',
-        backgroundColor: 'primary',
-        borderRadius: '4px',
+        width: '40px',
       }}
     >
-      {' '}
+      <Box
+        sx={{
+          cursor: 'pointer',
+          mb: '5px',
+          width: ['55%', '80%', '55%', '50%'],
+          ml: ['45%', '20%', '45%', '50%'],
+          height: '4px',
+          backgroundColor: 'primary',
+          borderRadius: '4px',
+        }}
+      />
+      <Box
+        sx={{
+          cursor: 'pointer',
+          mb: '5px',
+          width: ['70%', '100%', '70%', '66%'],
+          ml: ['30%', '0', '30%', '34%'],
+          height: '4px',
+          backgroundColor: 'primary',
+          borderRadius: '4px',
+        }}
+      >
+        {' '}
+      </Box>
+      <Box
+        sx={{
+          cursor: 'pointer',
+          width: ['45%', '50%', '50%', '45%'],
+          ml: ['55%', '50%', '50%', '55%'],
+          height: '4px',
+          backgroundColor: 'primary',
+          borderRadius: '4px',
+        }}
+      />
     </Box>
-    <Box
-      sx={{
-        cursor: 'pointer',
-        mb: '5px',
-        width: ['45%', '50%', '50%', '45%'],
-        ml: ['55%', '50%', '50%', '55%'],
-        height: '4px',
-        backgroundColor: 'primary',
-        borderRadius: '4px',
-      }}
-    />
-  </GridItem>
+  </Box>
 );
