536096118a65576eb49e9ac0e7a76e10a7b377a2
Carolin Maisenbacher <carolin@thepuzzlers.io>
2021-09-17 20:26:30 +0800

    Remove unused header section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,19 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { Header } from 'sections/landingPage/header/Header';
-import { Header as HeaderAngga } from 'sections/landingPage/header-angga';
 import { Values } from 'sections/landingPage/values/Values';
 import { Strengthen } from 'sections/landingPage/strengthen/Strengthen';
 import { Filler } from 'sections/landingPage/filler/Filler';
 import { Connections } from 'sections/landingPage/connections/Connections';
 import { Profile } from 'sections/landingPage/profile/Profile';
 import { StartGrowing } from 'sections/landingPage/growing/Growing';
-import potject from 'assets/jpg/header-sub-img-1.jpg';
-import greenThumb from 'assets/jpg/header-sub-img-2.jpg';
-import greenCafe from 'assets/jpg/header-sub-img-3.jpg';
 import FeaturePrice from 'sections/landingPage/featuresPrice';
 import { Contact } from 'sections/landingPage/contact/Contact';
 import Footer from 'sections/landingPage/footer';
 
-const headerData = {
-  firstCard: {
-    id: 1,
-    label: 'Potject',
-    location: 'Poetry in Bangkok',
-    img: `${potject}`,
-  },
-  secondCard: {
-    id: 2,
-    label: 'GreenThumb',
-    location: 'Gardener in CheChe',
-    img: `${greenThumb}`,
-  },
-  thirdCard: {
-    id: 3,
-    label: 'Green Cafe',
-    location: 'Roastery in Bangkok',
-    img: `${greenCafe}`,
-  },
-};
-
 const IndexPage = ({ data }) => (
   <div>
     <Header />
-    <HeaderAngga cardData={headerData} />
     <Strengthen />
     <Profile />
     <Connections />
--- src/sections/landingPage/header-angga/animations.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-unused-expressions */
