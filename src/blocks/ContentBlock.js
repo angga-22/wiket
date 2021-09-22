65c268b8fd0999ad4df4790c8a284e2e3f474a62
Carolin Maisenbacher <carolin@thepuzzlers.io>
2021-09-22 08:52:34 +0800

    Remove unused components

--- src/blocks/ContentBlock.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Block } from '@thepuzzlers/pieces';
-
-export const ContentBlock = ({ ...props }) => (
-  <Block variant='contentBlock' {...props} />
-);
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,1 @@
-export * from './buttons/buttons';
 export * from './cards/cards';
